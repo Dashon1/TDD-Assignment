785efb528faf56627c495bf06d05a920d9e15414
d-air1 <40647198+Dashon1@users.noreply.github.com>
2021-03-03 08:13:05 -0600

    Delete print_result.py

--- print_result.py
+++
@@ -1,6 +0,0 @@
-def print_result(results):
-    if (type(results) != tuple):
-        raise TypeError("Only tuples are accepted as a valid type to print")
-    else:
-        print("Your bmi is " + results[0] + " that puts you in the " + results[1] + " category.")
-        return ("Your bmi is " + results[0] + " that puts you in the " + results[1] + " category.")
