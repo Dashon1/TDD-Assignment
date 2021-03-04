341ad3f34cbf3e2f84e14f5615c5dde898dffdbd
d-air1 <40647198+Dashon1@users.noreply.github.com>
2021-03-04 08:41:17 -0600

    Delete unit_tests directory

--- unit_tests/set_weight.py
+++
@@ -1,9 +0,0 @@
-def set_weight(weight):
-    if (weight == 0):
-        raise ValueError("You can't weigh nothing.")
-    elif (weight < 0):
-        raise ValueError("I'm not a physicist, but I think it's impossible for a human to weigh less than nothing.")
-    elif (weight > 600):
-        raise ValueError("You can not weigh more than 600 pounds. If you weigh anywhere near this much you have bigger problems.")
-    else:
-        return True
